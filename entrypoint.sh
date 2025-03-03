9621103f2ff05965806052838c445c92eb7d2c84
Drew Thomasson <126999465+DrewThomasson@users.noreply.github.com>
2025-03-03 13:53:44 -0500

    now entrypoint.sh modify the requirements.txt file

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,18 +3,19 @@ set -e
 
 echo "Selected PyTorch variant: $PYTORCH_VARIANT"
 
+# Modify requirements.txt dynamically
 case "$PYTORCH_VARIANT" in
     cuda11)
-        pip install /pytorch_wheels/torch_cuda11.whl
+        TORCH_PACKAGE="torch==<CUDA11_VERSION>+cu11 torchvision torchaudio -f https://download.pytorch.org/whl/cu11/torch_stable.html"
         ;;
     cuda12)
-        pip install /pytorch_wheels/torch_cuda12.whl
+        TORCH_PACKAGE="torch==<CUDA12_VERSION>+cu12 torchvision torchaudio -f https://download.pytorch.org/whl/cu12/torch_stable.html"
         ;;
     amd)
-        pip install /pytorch_wheels/torch_amd.whl
+        TORCH_PACKAGE="torch==<AMD_VERSION> torchvision torchaudio -f https://download.pytorch.org/whl/rocm5.4/torch_stable.html"
         ;;
     intel)
-        pip install /pytorch_wheels/torch_intel.whl
+        TORCH_PACKAGE="torch==<INTEL_VERSION>"
         ;;
     *)
         echo "Invalid PYTORCH_VARIANT: $PYTORCH_VARIANT"
@@ -22,5 +23,12 @@ case "$PYTORCH_VARIANT" in
         ;;
 esac
 
-# Execute the command passed as arguments to the entrypoint.
+# Remove any existing torch entry and add the correct one
+sed -i '/^torch/d' requirements.txt
+echo "$TORCH_PACKAGE" >> requirements.txt
+
+# Install dependencies
+pip install --no-cache-dir -r requirements.txt
+
+# Execute the main command
 exec "$@"
